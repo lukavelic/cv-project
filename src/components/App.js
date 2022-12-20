6357e435a1cafd281abf744466a7af62d093d8ba
lukavelic <96191586+lukavelic@users.noreply.github.com>
2022-12-20 14:12:45 +0100

    Restructure folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './UI/Header';
 import Cv from './Cv';
 
 class App extends React.Component {
--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Cv.css';
-import Card from './Card';
+import Card from './UI/Card';
 
 class Cv extends React.Component {
     constructor(props) {
